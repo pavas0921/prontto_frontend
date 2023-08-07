5074bb7f44871e58f9e481e7b353b55d6eb06df7
Carlos Andrés Pavas Correa <pavas0921@gmail.com>
2023-08-07 00:00:26 -0500

    cambio backend url

--- src/services/rol.js
+++ src/services/rol.js
@@ -1,19 +1,19 @@
-//const API_BASE_URL = "https://prontto-backend.onrender.com";
-const API_BASE_URL = "http://localhost:4000";
+const API_BASE_URL = "https://prontto-backend.onrender.com";
+//const API_BASE_URL = "http://localhost:4000";
 
 export const getAllRolApi = async (token) => {
-    try {
-        const url = `${API_BASE_URL}/rol`;
-        const req = await fetch(url, {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-          });
-      
-      const data = await req.json();
-      return Promise.resolve(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    const url = `${API_BASE_URL}/rol`;
+    const req = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await req.json();
+    return Promise.resolve(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
